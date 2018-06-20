700847fe857ad8a899e73859b9ec76f42ada95ea
TartaricAcid <baka943@qq.com>
2018-06-20 20:54:55 +0800

    修正主脚本问题

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 
 import os
 import time
+import json
 
 import requests
 
 
 # 文件上传请求
 def upload(file_name='', display_name='', changelog=''):
-    headers = {'X-Api-Token': os.getenv('CFTOKEN')}
-    file = {'file': file_name}
-    metadata = {'meta': {"displayName": display_name,
-                         "changelog": changelog,
-                         "gameVersions": [
-                             6756
-                         ],
-                         "releaseType": "release"
-                         }
-                }
-    response = requests.post('http://httpbin.org/post', headers=headers, file=file, metadata=metadata)
+    header = {'X-Api-Token': os.getenv('CFTOKEN')}
+    meta = {'metadata': json.dumps({"displayName": display_name, "changelog": changelog, "gameVersions": [
+                                   6756], "releaseType": "release"}, ensure_ascii=False)}
+
+    response = requests.post('https://minecraft.curseforge.com/api/projects/268591/upload-file',
+                             headers=header, data=meta, files={'file': (file_name, open(file_name, 'rb'))})
     return response
 
 
@@ -56,16 +52,21 @@ def zip(path='.', version=''):
         # 剔除 en_us
         os.system('rm -f {}/project/assets/{}/lang/en_us.lang'.format(path, modid))
         # 再剔除 zh_cn_old
-        os.system('rm -f {}/project/assets/{}/lang/zh_cn_old.lang'.format(path, modid))
+        os.system(
+            'rm -f {}/project/assets/{}/lang/zh_cn_old.lang'.format(path, modid))
         # 最后判断文件是否为空
         if os.path.getsize('{}/project/assets/{}/lang/zh_cn.lang'.format(path, modid)) == 0:
             os.system('rm -rf {}/project/assets/{}'.format(path, modid))
 
     # 最后打包
-    os.system('mv {}/project/assets ./'.format(path))
+    os.system('mv {}/project/* ./'.format(path))
     os.system(
-        'zip -r -9 "Minecraft-Mod-Language-Modpack-{}.zip" "assets" "pack.mcmeta"  "pack.png" "README.md" "LICENSE"'.format(
+        'zip -r -9 "Minecraft-Mod-Language-Package-{}.zip" "assets" "pack.mcmeta"  "pack.png" "README.md" "LICENSE"'.format(
             version))
+    os.system('rm -rf ./Minecraft-Mod-Language-Package')
+    os.system('rm -rf ./assets')
+    os.system('rm -rf ./pack.png')
+    os.system('rm -rf ./pack.mcmeta')
 
 
 if __name__ == '__main__':
@@ -78,9 +79,10 @@ if __name__ == '__main__':
     # 上传
     file = 'Minecraft-Mod-Language-Package-{}.zip'.format(version_in)
     display = 'Minecraft-Mod-Language-Package-{}'.format(version_in)
-    log = '自动更新，本次更新时间：{}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    # r = upload(file_name=file, display_name=display, changelog=log)
+    log = '自动更新，本次更新时间：{}'.format(time.strftime(
+        "%Y-%m-%d %H:%M:%S", time.localtime()))
+    r = upload(file_name=file, display_name=display, changelog=log)
 
     # 存版本
-    # if r.status_code == 200:
-    #    version_set(version_in)
+    if (r.status_code == 200):
+        version_set(version_in)
